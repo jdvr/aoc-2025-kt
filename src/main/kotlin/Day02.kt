6beb181d3e710619f99d61c12f472a3aa0937584
jdvr <juandavidvegarguez@gmail.com>
2025-12-03 21:21:05 +0000

    day 2 refactor

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -5,13 +5,7 @@ fun main() {
     val DEBUG = false
     val debug = consoleDebug(DEBUG)
 
-
-    fun part1(lines: List<String>): Long {
-        val ranges = lines[0].split(",").map {
-            val (start, end) = it.split("-")
-            start.toLong() .. end.toLong()
-        }
-
+    fun part1(ranges: List<LongRange>): Long {
         val invalids = ranges.map { range ->
             range.filter { id ->
                 val asText = id.toString()
@@ -27,21 +21,20 @@ fun main() {
         return invalids.sum()
     }
 
-    fun part2(lines: List<String>): Long {
-        val ranges = lines[0].split(",").map {
-            val (start, end) = it.split("-")
-            start.toLong() .. end.toLong()
-        }
-
+    fun part2(ranges: List<LongRange>): Long {
         val invalids = ranges.map { range ->
             range.filter { id ->
                 val asText = id.toString()
                 val length = asText.length
                 // If the pattern is repeated at least twice, then pattern must be smaller than length/2
-                (1 .. length / 2).any { patternLength ->
-                    // 1, 2, 3...
-                    val pattern = asText.chunked(patternLength)
-                    pattern.distinct().size == 1
+                (1..length / 2).any { patternLength ->
+                    length % patternLength == 0 && run {
+                        // 1, 2, 3...
+                        val pattern = asText.substring(0, patternLength)
+                        (patternLength until length step patternLength).all { idx ->
+                            asText.substring(idx, idx + patternLength) == pattern
+                        }
+                    }
                 }
             }
         }.flatten()
@@ -51,19 +44,29 @@ fun main() {
         return invalids.sum()
     }
 
-    val res1Test = part1(readInput(dayId, isTest = true))
+    val testRanges = readInput(dayId, isTest = true)[0].split(",").map {
+        val (start, end) = it.split("-")
+        start.toLong()..end.toLong()
+    }
+
+    val ranges = readInput(dayId)[0].split(",").map {
+        val (start, end) = it.split("-")
+        start.toLong()..end.toLong()
+    }
+
+    val res1Test = part1(testRanges)
     println("Part 1: $res1Test")
     check(res1Test == 1227775554L)
-    val res1 = part1(readInput(dayId))
+    val res1 = part1(ranges)
     println("Part 1: $res1")
     check(res1 == 18893502033)
 
-    val res2Test = part2(readInput(dayId, isTest = true))
+    val res2Test = part2(testRanges)
     check(res2Test == 4174379265L) {
         "part 2 test failed with $res2Test"
     }
 
-    val res2 = part2(readInput(dayId))
+    val res2 = part2(ranges)
     println("Part 2: $res2")
     check(res2 == 26202168557) {
         "part 2 failed with $res2"
